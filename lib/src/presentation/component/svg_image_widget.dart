8b2dd5a112327ddbcfefb031c6b12bc7115069f9
Tran_Le_Minh_Tien <minhtientran8102@gmail.com>
2024-04-22 13:23:00 +0700

    Create ModalBottomSheet for Map UI

---
+++ lib/src/presentation/component/svg_image_widget.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class SvgImageWidget extends StatefulWidget {
+  String url;
+  double widthSize;
+  double heightSize;
+  Color color;
+  SvgImageWidget(
+      {super.key,
+      required this.url,
+      required this.heightSize,
+      required this.widthSize,
+      required this.color});
+
+  @override
+  State<SvgImageWidget> createState() => _SvgImageWidgetState();
+}
+
+class _SvgImageWidgetState extends State<SvgImageWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return ColorFiltered(
+        colorFilter: ColorFilter.mode(widget.color, BlendMode.srcIn),
+        child: SvgPicture.asset(widget.url,
+            width: widget.widthSize, height: widget.heightSize));
+  }
+}
--- lib/src/presentation/screen/map_page.dart
+++ lib/src/presentation/screen/map_page.dart
@@ -1,5 +1,8 @@
 import 'dart:developer' as developer;
 import 'package:flutter/material.dart';
+import 'package:gop_passenger/core/app_color.dart';
+import 'package:gop_passenger/core/app_icon.dart';
+import 'package:gop_passenger/src/presentation/component/svg_image_widget.dart';
 import 'package:vietmap_flutter_gl/vietmap_flutter_gl.dart';
 
 class MapPage extends StatefulWidget {
@@ -21,6 +24,14 @@ class _MapPageState extends State<MapPage> with TickerProviderStateMixin {
   LatLng? currentLatLng;
   String styleString =
       'https://maps.vietmap.vn/api/maps/light/styles.json?apikey=b00c56c4751ac6eb19dd72a48d9c8630d203e6aa8bbb104a';
+
+  // Bottom Navigation Bar
+  String sourceLocation = "";
+  String desLocation = "";
+
+  // Dimension
+  double width = 0;
+  double height = 0;
 
   void _onMapCreated(VietmapController controller) {
     setState(() {
@@ -75,5 +86,118 @@ class _MapPageState extends State<MapPage> with TickerProviderStateMixin {
             : const SizedBox()
       ]),
     );
+  }
+
+  void _locationBottomSheet() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      showModalBottomSheet(
+          context: context,
+          builder: (BuildContext context) {
+            return Container(
+                width: width,
+                constraints: BoxConstraints(maxHeight: height * 0.35),
+                margin: const EdgeInsets.only(left: 15, right: 15),
+                child: Column(children: [
+                  Container(
+                      height: 6,
+                      width: 50,
+                      margin: const EdgeInsets.only(top: 15, bottom: 15),
+                      decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(3))),
+                  _addressField(
+                      "PICKUP",
+                      sourceLocation,
+                      SvgImageWidget(
+                          color: Colors.red,
+                          url: AppIcon.icLocationCrosshairs,
+                          heightSize: width * 0.07,
+                          widthSize: width * 0.07)),
+                  _addressField(
+                      "DROP-OFF",
+                      desLocation,
+                      SvgImageWidget(
+                          color: Colors.green,
+                          url: AppIcon.icLocationDot,
+                          heightSize: width * 0.07,
+                          widthSize: width * 0.07)),
+                  const Spacer(),
+                  _btnApply(),
+                  const SizedBox(height: 25)
+                ]));
+          });
+    });
+  }
+
+  Widget _addressField(String title, String content, Widget widget) {
+    return Row(children: [
+      SizedBox(
+          width: width * 0.15,
+          height: width * 0.15,
+          child: Center(child: widget)),
+      const SizedBox(width: 10),
+      Expanded(
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        _addressName(title),
+        const SizedBox(height: 10),
+        _addressContent(content == "" ? "My curent location" : content),
+        Container(
+          color: Colors.grey,
+          width: width * 0.7,
+          height: 1.5,
+        )
+      ]))
+    ]);
+  }
+
+  Widget _addressName(String txt) {
+    return Text(txt, style: const TextStyle(fontSize: 18, color: Colors.grey));
+  }
+
+  Widget _addressContent(String txt) {
+    return Text(txt,
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
+        style: const TextStyle(fontSize: 24));
+  }
+
+  void _setDimension() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {
+        width = MediaQuery.of(context).size.width;
+        height = MediaQuery.of(context).size.height;
+      });
+    });
+  }
+
+  Widget _btnApply() {
+    return ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            backgroundColor: AppColor.primaryColor,
+            fixedSize: Size(width * 0.9, width * 0.1),
+            // side: const BorderSide(color: AppColor.primaryColor, width: 2),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(width * 0.075)),
+            padding: const EdgeInsets.all(0)),
+        onPressed: () {},
+        child: Text("Đặt",
+            style: TextStyle(
+                fontSize: width * 0.04,
+                fontWeight: FontWeight.bold,
+                color: Colors.white)));
+    ;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _setDimension();
+    _locationBottomSheet();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 }
