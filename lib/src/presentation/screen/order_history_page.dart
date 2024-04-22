cbee986cf353242d5eb0656535e0c4bb1263fb7c
Nguyễn Bá Phước <phcnguyenba@gmail.com>
2024-04-22 14:26:37 +0700

    Update order_history_page.dart

--- lib/src/presentation/screen/order_history_page.dart
+++ lib/src/presentation/screen/order_history_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class OrderHistoryPage extends StatelessWidget {
   const OrderHistoryPage({Key? key}) : super(key: key);
@@ -9,172 +10,105 @@ class OrderHistoryPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Order History'),
       ),
-      body: Center(
+      body: const Center(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Container(
-              width: 360,
-              // padding: EdgeInsets.all(10), // Thêm padding cho Container
-              child: const Row(
-                children: <Widget>[
-                  Flexible(
-                    child: Card(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Flexible(
-                                child: Padding(
-                                  padding: EdgeInsets.only(left: 10, top: 10, bottom: 10, right: 10), // Thêm padding cho Text
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: <Widget>[
-                                      Text(
-                                        "Trường Đại Học Sư Phạm Kỹ Thuật Thành Phố Hồ Chí Minh",
-                                        style: TextStyle(
-                                          fontSize: 18,
-                                          color: Colors.black,
-                                        ),
-                                      ),
-                                      SizedBox(height: 5), // Thêm khoảng cách giữa hai Text
-                                      Text(
-                                        "1 thg 1 2024, 16:20",
-                                        style: TextStyle(
-                                          fontSize: 14,
-                                          color: Colors.black,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                              Text(
-                                "40.000đ" + "   ",
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  color: Colors.black,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+            OderHistoryCardItem(
+              dropoffAddress: 'Trường ĐH SPKT TP.HCM',
+              orderTime: '2021-10-10 10:10:10',
+              tripFare: '100000',
             ),
-
-            Container(
-              width: 360,
-              // padding: EdgeInsets.all(10), // Thêm padding cho Container
-              child: const Row(
-                children: <Widget>[
-                  Flexible(
-                    child: Card(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Flexible(
-                                child: Padding(
-                                  padding: EdgeInsets.only(left: 10, top: 10, bottom: 10, right: 10), // Thêm padding cho Text
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: <Widget>[
-                                      Text(
-                                        "Đường số 8, Linh Xuân, Thủ Đức",
-                                        style: TextStyle(
-                                          fontSize: 18,
-                                          color: Colors.black,
-                                        ),
-                                      ),
-                                      SizedBox(height: 5), // Thêm khoảng cách giữa hai Text
-                                      Text(
-                                        "1 thg 1 2024, 16:20",
-                                        style: TextStyle(
-                                          fontSize: 14,
-                                          color: Colors.black,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                              Text(
-                                "40.000đ" + "   ",
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  color: Colors.black,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+            OderHistoryCardItem(
+              dropoffAddress: 'cafe izy',
+              orderTime: '2021-10-10 10:10:10',
+              tripFare: '24000',
             ),
+            OderHistoryCardItem(
+              dropoffAddress: 'sân bay Tân Sơn Nhất',
+              orderTime: '2021-10-10 10:10:10',
+              tripFare: '53000',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-            Container(
-              width: 360,
-              // padding: EdgeInsets.all(10), // Thêm padding cho Container
-              child: const Row(
-                children: <Widget>[
-                  Flexible(
-                    child: Card(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Flexible(
-                                child: Padding(
-                                  padding: EdgeInsets.only(left: 10, top: 10, bottom: 10, right: 10), // Thêm padding cho Text
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: <Widget>[
-                                      Text(
-                                        "484A Lê Văn Việt, Phường Tăng Nhơn Phú A, Quận 9",
-                                        style: TextStyle(
-                                          fontSize: 18,
-                                          color: Colors.black,
-                                        ),
-                                      ),
-                                      SizedBox(height: 5), // Thêm khoảng cách giữa hai Text
-                                      Text(
-                                        "1 thg 1 2024, 16:20",
-                                        style: TextStyle(
-                                          fontSize: 14,
-                                          color: Colors.black,
-                                        ),
-                                      ),
-                                    ],
+class OderHistoryCardItem extends StatelessWidget {
+  const OderHistoryCardItem({
+    super.key,
+    required this.dropoffAddress,
+    required this.orderTime,
+    required this.tripFare,
+  });
+
+  final String dropoffAddress;
+  final String orderTime;
+  final String tripFare;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        context.push('/orders/orderhistoryitem');
+      },
+      child: SizedBox(
+        width: 360,
+        // padding: EdgeInsets.all(10), // Thêm padding cho Container
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: Card(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Flexible(
+                          child: Padding(
+                            padding: EdgeInsets.only(
+                                left: 10,
+                                top: 10,
+                                bottom: 10,
+                                right: 10), // Thêm padding cho Text
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  dropoffAddress,
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.black,
                                   ),
                                 ),
-                              ),
-                              Text(
-                                "40.000đ" + "   ",
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  color: Colors.black,
+                                SizedBox(
+                                    height:
+                                        5), // Thêm khoảng cách giữa hai Text
+                                Text(
+                                  orderTime,
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.black,
+                                  ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
-                        ],
-                      ),
+                        ),
+                        Text(
+                          "$tripFare VND",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ],
