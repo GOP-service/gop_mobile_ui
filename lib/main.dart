0cf2697ff7798b82ffa807c4354ad55ac2bd9118
Tran_Le_Minh_Tien <minhtientran8102@gmail.com>
2024-04-25 01:09:03 +0700

    Update customer

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,38 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
-import 'package:gop_passenger/core/app_color.dart';
-import 'package:gop_passenger/core/dio_manager.dart';
-import 'package:gop_passenger/router.dart';
-import 'package:gop_passenger/src/bloc/auth/auth_bloc.dart';
-import 'package:gop_passenger/src/data/repository/auth_repository.dart';
-import 'package:gop_passenger/src/data/data_source/auth_controller.dart';
-import 'package:gop_passenger/src/presentation/widgets/loading_overlay.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:timezone/data/latest.dart' as tz;
+
+import '/service/firebase_service.dart';
+import '/view/navigation.dart' show NavigationChange;
+
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await FirebaseService.initializeApp();
+  tz.initializeTimeZones();
   runApp(const MyApp());
-
-  SystemChrome.setPreferredOrientations(
-      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-    statusBarColor: AppColor.primaryColor,
-    statusBarIconBrightness: Brightness.light,
-    systemNavigationBarColor: AppColor.whiteColor,
-    systemNavigationBarIconBrightness: Brightness.dark,
-  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  void showSnackBar(BuildContext context, String message) =>
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(message),
-          duration: const Duration(seconds: 2),
-          backgroundColor: AppColor.redColor,
-        ),
-      );
+
+class MyApp extends StatefulWidget {
+  const MyApp({ Key? key }) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return RepositoryProvider(
-      create: (context) => AuthRepository(),
-      child: BlocProvider(
-        create: (context) => AuthBloc(context.read<AuthRepository>()),
-        child: MaterialApp.router(
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-              scaffoldBackgroundColor: AppColor.lightGreyColor,
-              useMaterial3: true,
-              fontFamily: 'Nunito'),
-          routerConfig: appRouter,
-        ),
-      ),
+
+    return const MaterialApp(
+      title: "Taxi App For Customer",
+      home: NavigationChange()
     );
   }
 }
+
+
